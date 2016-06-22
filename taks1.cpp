f4f6b33546726900880bbfa2ab80d92bb027cf3a
setlol <dovnar.maxim@gmail.com>
2016-06-22 19:14:51 +0300

    g
    добавлена картинка g

--- taks1.cpp
+++ taks1.cpp
@@ -192,7 +192,31 @@ int main()
 	}
 		break;
 	case 'g':
-		cout << total;
+	{
+		space1 = width;
+		while (cycle <= width)
+		{
+			while (total <= width)
+			{
+				if (total > space ||
+					total > space1)
+				{
+					cout << ' ';
+				}
+				else
+				{
+					cout << '*';
+				}
+				total++;
+			}
+			cout << endl;
+			total = 1;
+			space++;
+			space1--;
+
+			cycle++;
+		}
+	}
 		break;
 	case 'h':
 		cout << total;
